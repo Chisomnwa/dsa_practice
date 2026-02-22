ab132d372f9b4c2d940e1374c0f2fa78229af888
cnnamani <nnamanichisomnc@gmail.com>
2026-02-22 10:38:04 +0100

    Cretaed the main.go file to call the go functions

--- main.go
+++ main.go
@@ -1,2 +1,13 @@
 package main
 
+import (
+	"fmt";
+	"piscine/go-solutions"
+)
+
+
+func main() {
+	fmt.Println(piscine.IsPalindrome("A man, a plan, a canal: Panama"))
+	fmt.Println(piscine.IsPalindrome(" "))
+	fmt.Println(piscine.IsPalindrome("race a car"))
+}
