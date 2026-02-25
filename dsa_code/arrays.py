4e58032807a4dfb70b195d3213e3a9de2003a16a
cnnamani <nnamanichisomnc@gmail.com>
2026-02-25 17:47:22 +0100

    Add files to GitHub

---
+++ dsa_code/arrays.py
@@ -0,0 +1,15 @@
+new_list = [1, 2, 3]
+
+# Accessing an Array
+result = new_list[0]
+print(result)
+
+# Searching for an arbitral value in an array
+# using an if srtatement
+if 1 in new_list: print(True)
+
+# Using a for loop
+for n in new_list:
+    if n == 1:
+        print(True)
+    break
