cc091eefab07ad708d27f27787eacf625446e7b0
cnnamani <nnamanichisomnc@gmail.com>
2026-02-25 10:13:33 +0100

    Added a go solution for Valid Palindrome II question

--- go-solutions/ispalindromeII.go
+++ go-solutions/ispalindromeII.go
@@ -1,8 +1,34 @@
 package gosolutions
 
-func isPalindromeII () {
+func IsPalindromeII (s string) bool{
+	/*
+		This function takes in a string (made up of lowercase English letters) 
+		and returns true if the string can be palindrome after deleting at most
+		one character from it.
+	*/
+	left := 0
+	right := len(s) - 1
 
-
+	for left < right {
+		if s[left] == s[right] {
+			left++
+			right--
+		} else {
+			// Try skipping one character
+			return IsPalindromeRange(s, left+1, right) ||
+				IsPalindromeRange(s, left, right-1)
+		}
+	}
+	return true
 }
 
-
+func IsPalindromeRange(s string, left, right int) bool {
+	for left < right {
+		if s[left] != s[right] {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
+}
--- main.go
+++ main.go
@@ -10,6 +10,9 @@ func main() {
 	// fmt.Println(gosolutions.IsPalindrome(" "))
 	// fmt.Println(gosolutions.IsPalindrome("race a car"))
 	// fmt.Println(gosolutions.RomanToInt("IV"))
-	fmt.Println(gosolutions.DefangIPaddr("1.1.1.1"))
+	// fmt.Println(gosolutions.DefangIPaddr("1.1.1.1"))
+	fmt.Println(gosolutions.IsPalindromeII("aba"))
+	fmt.Println(gosolutions.IsPalindromeII("abca"))
+	fmt.Println(gosolutions.IsPalindromeII("abc"))
 }
 
