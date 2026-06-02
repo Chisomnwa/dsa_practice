d4171034f0c8315315e1b3233551305a0b1752ba
cnnamani <nnamanichisomnc@gmail.com>
2026-06-02 09:49:08 +0100

    docs: update walkthrough steps for brute force solution.

--- arrays/build_array_from_permutation/2026_05_28_build_array_from_permutation_A.go
+++ arrays/build_array_from_permutation/2026_05_28_build_array_from_permutation_A.go
@@ -60,15 +60,15 @@ Step 1: We create ans; the new array, same length as nums
 Step 2: Loop through nums using index i
 
 	The first iteration
-		when i = 0, nums[0] = nums[nums[0]] → 0 → ans[0] = 0
-		when i = 1, nums[1] = nums[nums[1]] → 2 → ans[1] = 1
-		when i = 2, nums[2] = nums[nums[2]] → 1 → ans[2] = 2
-		when i = 3, nums[3] = nums[nums[3]] → 5 → ans[3] = 4
-		when i = 4, nums[4] = nums[nums[4]] → 3 → ans[4] = 5
-		when i = 5, nums[5] = nums[nums[5]] → 4 → ans[5] = 3
+		when i = 0, nums[nums[0]] = nums[0] → 0 → ans[0] = 0
+		when i = 1, nums[nums[1]] = nums[2] → 2 → ans[1] = 1
+		when i = 2, nums[nums[2]] = nums[1] → 1 → ans[2] = 2
+		when i = 3, nums[nums[3]] = nums[5] → 5 → ans[3] = 4
+		when i = 4, nums[nums[4]] = nums[3] → 3 → ans[4] = 5
+		when i = 5, nums[nums[5]] = nums[4] → 4 → ans[5] = 3
 
-step 3: Retuen array [0,1,2,4,5,3]
+step 3: Return array [0,1,2,4,5,3]
 
 Time Complexity : O(N) - we loop through all the values in the array at once
-Space complexity: O(N) - we created an extra data structure 'ans' (the new array)
+Space Complexity : O(N) - we created an extra data structure 'ans' (the new array)
 */
